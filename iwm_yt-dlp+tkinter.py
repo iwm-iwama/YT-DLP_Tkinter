0bf0f0f307e1fa8e4a09b4acff59d0cbba455754
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2024-05-13 11:19:58 +0900

    Add files via upload

--- iwm_yt-dlp+tkinter.py
+++ iwm_yt-dlp+tkinter.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #coding:utf-8
 
 PROGRAM = "YT-DLP+Tkinter"
-VERSION = "Ver.iwm20240504"
+VERSION = "Ver.iwm20240513"
 
 import shutil
 import subprocess
+import time
 import tkinter as Tk
 import tkinter.scrolledtext as Tk_St
 import tkinter.ttk as Tk_Ttk
@@ -18,7 +19,7 @@ from tkinter import messagebox
 # My Config
 #-------------------------------------------------------------------------------
 # YT-DLP Command & Option
-CmdOpt = """
+List_Cmd = """
 yt-dlp -f b
 yt-dlp -x --audio-format mp3
 echo
@@ -26,11 +27,17 @@ yt-dlp --help
 """
 
 #-------------------------------------------------------------------------------
+# Const
+#-------------------------------------------------------------------------------
+H1_BGN = "\033[97;104m$ " + (" " * 70) + "\033[3G"
+H1_END = "\033[49m"
+
+#-------------------------------------------------------------------------------
 # Function
 #-------------------------------------------------------------------------------
 def Sub_Clear():
 	# おまじない
-	subprocess.run("clear || cls", shell=True)
+	subprocess.run("clear || cls", shell = True)
 
 def Sub_SimpleHelp():
 	print(
@@ -61,13 +68,14 @@ def Sub_SimpleHelp():
 def Sub_YtDlp_Update():
 	Cmd = "yt-dlp"
 	if shutil.which(Cmd):
+		# ダイアログは最前面に"固定表示されない"仕様のようなので注意!!!!
 		Select = messagebox.askyesno(PROGRAM, "YT-DLP の更新を確認しますか ?")
 		if Select == True:
 			print(
 				"\033[97;104m " + Cmd + " \033[0m" +
 				"\n" +
 				"    " +
-				"\033[97m" + subprocess.run((Cmd + " --update-to nightly"), shell=True, capture_output=True, text=True).stdout.strip().replace("\n", "\n    ") +
+				"\033[97m" + subprocess.run((Cmd + " --update-to nightly"), shell = True, capture_output = True, text = True).stdout.strip().replace("\n", "\n    ") +
 				"\n" +
 				"\033[97;104m (END) \033[0m" +
 				"\n"
@@ -93,14 +101,14 @@ def Sub_Terminal_Reposition():
 
 def Sub_Root_Resize(e):
 	if e.widget is Root:
-		CmdOpt_Cb1.place(width=e.width-152)
-		CmdOpt_Exec_Btn1.place(x=e.width-147)
-		Args_St1.place(width=e.width-10, height=e.height-79)
-		Args_Clear_Btn1.place(x=e.width-147)
-		Args_Paste_Btn1.place(x=e.width-85)
+		List_Cmd_Cb1.place(width = e.width - 152)
+		List_Cmd_Exec_Btn1.place(x = e.width - 147)
+		Args_St1.place(width = e.width - 10, height = e.height - 79)
+		Args_Clear_Btn1.place(x = e.width - 147)
+		Args_Paste_Btn1.place(x = e.width - 85)
 
-def Sub_CmdOpt_Cb1_ContextMenu(e):
-	obj1 = CmdOpt_Cb1
+def Sub_List_Cmd_Cb1_ContextMenu(e):
+	obj1 = List_Cmd_Cb1
 	obj2 = None
 	if obj1.selection_present() == True:
 		obj2 = Cb_ContextMenu_Select
@@ -108,21 +116,21 @@ def Sub_CmdOpt_Cb1_ContextMenu(e):
 		obj2 = Cb_ContextMenu_All
 	obj2.post(e.x_root, e.y_root)
 
-def Sub_Cb_Clear_Select(obj=None, e=None):
+def Sub_Cb_Clear_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		obj.delete("sel.first", "sel.last")
 	return inner
 
-def Sub_Cb_Clear_All(obj=None, e=None):
+def Sub_Cb_Clear_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		obj.delete("0", "end")
 	return inner
 
-def Sub_Cb_Copy_Select(obj=None, e=None):
+def Sub_Cb_Copy_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -130,7 +138,7 @@ def Sub_Cb_Copy_Select(obj=None, e=None):
 		obj.clipboard_append(obj.selection_get())
 	return inner
 
-def Sub_Cb_Copy_All(obj=None, e=None):
+def Sub_Cb_Copy_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -138,7 +146,7 @@ def Sub_Cb_Copy_All(obj=None, e=None):
 		obj.clipboard_append(obj.get())
 	return inner
 
-def Sub_Cb_Cut_Select(obj=None, e=None):
+def Sub_Cb_Cut_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -147,7 +155,7 @@ def Sub_Cb_Cut_Select(obj=None, e=None):
 		obj.delete("sel.first", "sel.last")
 	return inner
 
-def Sub_Cb_Cut_All(obj=None, e=None):
+def Sub_Cb_Cut_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -156,57 +164,68 @@ def Sub_Cb_Cut_All(obj=None, e=None):
 		obj.delete("0", "end")
 	return inner
 
-def Sub_Cb_Paste_Select(obj=None, e=None):
+def Sub_Cb_Paste_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		try:
-			text = Root.selection_get(selection="CLIPBOARD").rstrip()
+			text = Root.selection_get(selection = "CLIPBOARD").rstrip()
 			obj.delete("sel.first", "sel.last")
 			obj.insert("insert", text)
 		except:
 			pass
 	return inner
 
-def Sub_Cb_Paste_All(obj=None, e=None):
+def Sub_Cb_Paste_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		try:
-			text = Root.selection_(selection="CLIPBOARD").rstrip()
+			text = Root.selection_(selection = "CLIPBOARD").rstrip()
 			obj.insert("insert", text)
 		except:
 			pass
 	return inner
 
-def Sub_CmdOpt_Exec_Btn1(e=None):
+def Sub_List_Cmd_Exec_Btn1(e = None):
 	obj1 = Args_St1
-	obj2 = CmdOpt_Cb1
+	obj2 = List_Cmd_Cb1
 	s1 = obj1.get("1.0", "end-1c").strip()
 	s2 = obj2.get().strip()
 	a1 = []
 	if len(s1) > 0:
-		for _ln in s1.split("\n"):
-			_ln = _ln.strip()
-			if len(_ln) > 0:
-				a1 += [(s2 + " " + _ln)]
+		for _opt in s1.split("\n"):
+			_opt = _opt.strip()
+			if len(_opt) > 0:
+				a1 += [(s2 + " " + _opt)]
 	else:
 		a1 += [s2]
 	Sub_Clear()
 	Cnt = 0
+	List_PS = []
+	# PS実行リスト作成
 	for _s1 in a1:
-		_s1 = _s1.strip()
-		print("\033[97;104m " + _s1 + " \033[0m")
+		print(H1_BGN + _s1 + H1_END)
 		try:
-			subprocess.run(_s1, shell=True)
+			List_PS.append(subprocess.Popen(_s1, shell = True))
 			Cnt += 1
 		except:
 			break
-	a1 = []
-	AddStr = str(Cnt) + " Count"
+	i1 = Cnt
+	# PS監視
+	for _ps in List_PS:
+		###print("Existing PS: " + str(_ps))
+		while _ps.poll() is None:
+			###print("Waiting PS: " + str(_ps))
+			time.sleep(1.0)
+		i1 -= 1
+		###print(F"Remaining PS Count: {i1}")
+		time.sleep(1.0)
+	# PS終了処理
+	AddStr = "(END) " + str(Cnt) + " Count"
 	if Cnt > 1:
 		AddStr += "s"
-	print("\n" + "\033[97;104m (END) " + AddStr + " \033[0m")
+	print(H1_BGN + AddStr + H1_END)
 
 def Sub_Args_St1_ContextMenu(e):
 	obj1 = Args_St1
@@ -217,21 +236,21 @@ def Sub_Args_St1_ContextMenu(e):
 		obj2 = St_ContextMenu_All
 	obj2.post(e.x_root, e.y_root)
 
-def Sub_St_Clear_Select(obj=None, e=None):
+def Sub_St_Clear_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		obj.delete("sel.first", "sel.last")
 	return inner
 
-def Sub_St_Clear_All(obj=None, e=None):
+def Sub_St_Clear_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		obj.delete("1.0", "end")
 	return inner
 
-def Sub_St_Copy_Select(obj=None, e=None):
+def Sub_St_Copy_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -239,7 +258,7 @@ def Sub_St_Copy_Select(obj=None, e=None):
 		obj.clipboard_append(obj.get("sel.first", "sel.last"))
 	return inner
 
-def Sub_St_Copy_All(obj=None, e=None):
+def Sub_St_Copy_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -247,7 +266,7 @@ def Sub_St_Copy_All(obj=None, e=None):
 		obj.clipboard_append(obj.get("1.0", "end-1c"))
 	return inner
 
-def Sub_St_Cut_Select(obj=None, e=None):
+def Sub_St_Cut_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -256,7 +275,7 @@ def Sub_St_Cut_Select(obj=None, e=None):
 		obj.delete("sel.first", "sel.last")
 	return inner
 
-def Sub_St_Cut_All(obj=None, e=None):
+def Sub_St_Cut_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
@@ -265,12 +284,12 @@ def Sub_St_Cut_All(obj=None, e=None):
 		obj.delete("1.0", "end")
 	return inner
 
-def Sub_St_Paste_Select(obj=None, e=None):
+def Sub_St_Paste_Select(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		try:
-			text = Root.selection_get(selection="CLIPBOARD").rstrip()
+			text = Root.selection_get(selection = "CLIPBOARD").rstrip()
 			obj.delete("sel.first", "sel.last")
 			obj.insert("insert", text + "\n")
 			obj.see("insert")
@@ -278,12 +297,12 @@ def Sub_St_Paste_Select(obj=None, e=None):
 			pass
 	return inner
 
-def Sub_St_Paste_All(obj=None, e=None):
+def Sub_St_Paste_All(obj = None, e = None):
 	if obj == None:
 		return
 	def inner():
 		try:
-			text = Root.selection_get(selection="CLIPBOARD").rstrip()
+			text = Root.selection_get(selection = "CLIPBOARD").rstrip()
 			obj.insert("insert", text + "\n")
 			obj.see("insert")
 		except:
@@ -294,10 +313,11 @@ def Sub_St_Paste_All(obj=None, e=None):
 # Window Root
 #-------------------------------------------------------------------------------
 Root = Tk.Tk()
+
 # Window 初期サイズ
 min = {
 	"W": 480,
-	"H": 300
+	"H": 200
 }
 # Window 初期ポジション
 pos = {
@@ -305,91 +325,93 @@ pos = {
 	"Y": int((Root.winfo_screenheight() - min["H"]) / 2)
 }
 Root.bind("<Configure>", Sub_Root_Resize)
-Root.configure(bg="#555")
+Root.configure(bg = "#555")
 Root.geometry(f'{min["W"]}x{min["H"]}+{pos["X"]}+{pos["Y"]}')
-Root.minsize(width=min["W"], height=min["H"])
-Root.resizable(width=True, height=True)
+Root.minsize(width = min["W"], height = min["H"])
+Root.resizable(width = True, height = True)
 Root.title(PROGRAM + " " + VERSION)
+# 使用不可：Windows,Linux互換問題
+#   Root.attributes()
 
 #-------------------------------------------------------------------------------
 # Command & Option
 #-------------------------------------------------------------------------------
-CmdOpt_Lbl1 = Tk.Label(text="YT-DLP コマンド／オプション", font=("Helvetica", 10, "bold"), fg="white", bg="#555")
+List_Cmd_Lbl1 = Tk.Label(text = "YT-DLP コマンド", font = ("Helvetica", 10, "bold"), fg = "white", bg = "#555")
 
-a1 = CmdOpt.strip().split("\n")
-CmdOpt_Cb1 = Tk_Ttk.Combobox(Root, font=("Courier", 10), values=(a1))
-obj1 = CmdOpt_Cb1
-obj1.bind("<Button-3>", Sub_CmdOpt_Cb1_ContextMenu)
+a1 = List_Cmd.strip().split("\n")
+List_Cmd_Cb1 = Tk_Ttk.Combobox(Root, font = ("Courier", 10), values = (a1))
+obj1 = List_Cmd_Cb1
+obj1.bind("<Button-3>", Sub_List_Cmd_Cb1_ContextMenu)
 obj1.insert("end", a1[0])
 a1 = []
 
 # 範囲指定あり
-Cb_ContextMenu_Select = Tk.Menu(Root, tearoff=0, font=("Helvetica", 10))
+Cb_ContextMenu_Select = Tk.Menu(Root, tearoff = 0, font = ("Helvetica", 10))
 obj1 = Cb_ContextMenu_Select
-obj1.add_command(label="クリア", command=Sub_Cb_Clear_Select(obj=CmdOpt_Cb1))
+obj1.add_command(label = "クリア", command = Sub_Cb_Clear_Select(obj = List_Cmd_Cb1))
 obj1.add_separator()
-obj1.add_command(label="コピー", command=Sub_Cb_Copy_Select(obj=CmdOpt_Cb1))
-obj1.add_command(label="カット", command=Sub_Cb_Cut_Select(obj=CmdOpt_Cb1))
-obj1.add_command(label="ペースト", command=Sub_Cb_Paste_Select(obj=CmdOpt_Cb1))
+obj1.add_command(label = "コピー", command = Sub_Cb_Copy_Select(obj = List_Cmd_Cb1))
+obj1.add_command(label = "カット", command = Sub_Cb_Cut_Select(obj = List_Cmd_Cb1))
+obj1.add_command(label = "ペースト", command = Sub_Cb_Paste_Select(obj = List_Cmd_Cb1))
 
 # 範囲指定なし
-Cb_ContextMenu_All = Tk.Menu(Root, tearoff=0, font=("Helvetica", 10))
+Cb_ContextMenu_All = Tk.Menu(Root, tearoff = 0, font = ("Helvetica", 10))
 obj1 = Cb_ContextMenu_All
-obj1.add_command(label="全クリア", command=Sub_Cb_Clear_All(obj=CmdOpt_Cb1))
+obj1.add_command(label = "全クリア", command = Sub_Cb_Clear_All(obj = List_Cmd_Cb1))
 obj1.add_separator()
-obj1.add_command(label="全コピー", command=Sub_Cb_Copy_All(obj=CmdOpt_Cb1))
-obj1.add_command(label="全カット", command=Sub_Cb_Cut_All(obj=CmdOpt_Cb1))
-obj1.add_command(label="ペースト", command=Sub_Cb_Paste_All(obj=CmdOpt_Cb1))
+obj1.add_command(label = "全コピー", command = Sub_Cb_Copy_All(obj = List_Cmd_Cb1))
+obj1.add_command(label = "全カット", command = Sub_Cb_Cut_All(obj = List_Cmd_Cb1))
+obj1.add_command(label = "ペースト", command = Sub_Cb_Paste_All(obj = List_Cmd_Cb1))
 
-CmdOpt_Exec_Btn1 = Tk.Button(Root, text="実行", font=("Helvetica", 10), fg="#ffffff", bg="crimson", relief="flat", cursor="hand2", command=Sub_CmdOpt_Exec_Btn1)
+List_Cmd_Exec_Btn1 = Tk.Button(Root, text = "実行", font = ("Helvetica", 10), fg = "#ffffff", bg = "crimson", relief = "flat", cursor = "hand2", command = Sub_List_Cmd_Exec_Btn1)
 
 #-------------------------------------------------------------------------------
 # Argument
 #-------------------------------------------------------------------------------
-Args_Lbl1 = Tk.Label(text="YouTube URL／改行区切り", font=("Helvetica", 10, "bold"), fg="white", bg="#555")
+Args_Lbl1 = Tk.Label(text = "YouTube URL（改行区切り）", font = ("Helvetica", 10, "bold"), fg = "white", bg = "#555")
 
-Args_St1 = Tk_St.ScrolledText(Root, font=("Courier", 11), relief="flat", borderwidth=0, undo="true", insertofftime=0)
+Args_St1 = Tk_St.ScrolledText(Root, font = ("Courier", 11), relief = "flat", borderwidth = 0, undo = "true", insertofftime = 0)
 obj1 = Args_St1
 obj1.bind("<Button-3>", Sub_Args_St1_ContextMenu)
-obj1.configure(state="normal")
+obj1.configure(state = "normal")
 
 # 範囲指定あり
-St_ContextMenu_Select = Tk.Menu(Root, font=("Helvetica", 10), tearoff=0)
+St_ContextMenu_Select = Tk.Menu(Root, font = ("Helvetica", 10), tearoff = 0)
 obj1 = St_ContextMenu_Select
-obj1.add_command(label="クリア", command=Sub_St_Clear_Select(obj=Args_St1))
+obj1.add_command(label = "クリア", command = Sub_St_Clear_Select(obj = Args_St1))
 obj1.add_separator()
-obj1.add_command(label="コピー", command=Sub_St_Copy_Select(obj=Args_St1))
-obj1.add_command(label="カット", command=Sub_St_Cut_Select(obj=Args_St1))
-obj1.add_command(label="ペースト", command=Sub_St_Paste_Select(obj=Args_St1))
+obj1.add_command(label = "コピー", command = Sub_St_Copy_Select(obj = Args_St1))
+obj1.add_command(label = "カット", command = Sub_St_Cut_Select(obj = Args_St1))
+obj1.add_command(label = "ペースト", command = Sub_St_Paste_Select(obj = Args_St1))
 
 # 範囲指定なし
-St_ContextMenu_All = Tk.Menu(Root, font=("Helvetica", 10), tearoff=0)
+St_ContextMenu_All = Tk.Menu(Root, font = ("Helvetica", 10), tearoff = 0)
 obj1 = St_ContextMenu_All
-obj1.add_command(label="全クリア", command=Sub_St_Clear_All(obj=Args_St1))
+obj1.add_command(label = "全クリア", command = Sub_St_Clear_All(obj = Args_St1))
 obj1.add_separator()
-obj1.add_command(label="全コピー", command=Sub_St_Copy_All(obj=Args_St1))
-obj1.add_command(label="全カット", command=Sub_St_Cut_All(obj=Args_St1))
-obj1.add_command(label="ペースト", command=Sub_St_Paste_All(obj=Args_St1))
+obj1.add_command(label = "全コピー", command = Sub_St_Copy_All(obj = Args_St1))
+obj1.add_command(label = "全カット", command = Sub_St_Cut_All(obj = Args_St1))
+obj1.add_command(label = "ペースト", command = Sub_St_Paste_All(obj = Args_St1))
 
-Args_Clear_Btn1 = Tk.Button(Root, text="クリア", font=("Helvetica", 9), fg="white", bg="navy", relief="flat", cursor="hand2", command=Sub_St_Clear_All(obj=Args_St1))
-Args_Paste_Btn1 = Tk.Button(Root, text="ペースト", font=("Helvetica", 9), fg="white", bg="mediumblue", relief="flat", cursor="hand2", command=Sub_St_Paste_All(obj=Args_St1))
+Args_Clear_Btn1 = Tk.Button(Root, text = "クリア", font = ("Helvetica", 9), fg = "white", bg = "navy", relief = "flat", cursor = "hand2", command = Sub_St_Clear_All(obj = Args_St1))
+Args_Paste_Btn1 = Tk.Button(Root, text = "ペースト", font = ("Helvetica", 9), fg = "white", bg = "mediumblue", relief = "flat", cursor = "hand2", command = Sub_St_Paste_All(obj = Args_St1))
 
 #-------------------------------------------------------------------------------
 # Main
 #-------------------------------------------------------------------------------
-CmdOpt_Lbl1.place(x=5, y=3)
-CmdOpt_Cb1.place(x=5, y=23, height=20)
-CmdOpt_Exec_Btn1.place(y=23, width=62, height=20)
-Args_Lbl1.place(x=5, y=53)
-Args_St1.place(x=5, y=73)
-Args_Clear_Btn1.place(y=53, width=62, height=20)
-Args_Paste_Btn1.place(y=53, width=80, height=20)
+List_Cmd_Lbl1.place(x = 5, y = 3)
+List_Cmd_Cb1.place(x = 5, y = 23, height = 20)
+List_Cmd_Exec_Btn1.place(y = 23, width = 62, height = 20)
+Args_Lbl1.place(x = 5, y = 53)
+Args_St1.place(x = 5, y = 73)
+Args_Clear_Btn1.place(y = 53, width = 62, height = 20)
+Args_Paste_Btn1.place(y = 53, width = 80, height = 20)
 
 # 前処理
 Sub_Terminal_Reposition()
 Sub_Clear()
 Sub_YtDlp_Update()
-CmdOpt_Cb1.focus_force()
+List_Cmd_Cb1.focus_force()
 
 Root.mainloop()
 Root.quit()
